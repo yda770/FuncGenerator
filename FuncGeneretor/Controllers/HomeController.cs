4fc29ccf72282fd6ae9c1d8e748aa2b35670a2e9
yda770 <35491939+yda770@users.noreply.github.com>
2019-07-29 21:23:40 +0300

    first commit

--- FuncGeneretor/Controllers/HomeController.cs
+++ FuncGeneretor/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CSASPNETHighlightCodeInPage;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,6 +12,28 @@ namespace FuncGeneretor.Controllers
     {
         public ActionResult Index()
         {
+            string InputText = "var x = 10;\nx *= 5;\ndocument.getElementById(\"demo\").innerHTML = x;\nif( x == 8 ){\n}\nelse{\nwhile(true)//111test comment\n}";
+            Hashtable _htb = CSASPNETHighlightCodeInPage.CodeManager.Init();
+
+            // Initialize the suitable collection object.
+            RegExp _rg = new RegExp();
+            _rg = (RegExp)_htb["js"];
+
+            // Display the highlighted code in a label control.
+            ViewBag.GeneratedCode = CodeManager.Encode(
+                CodeManager.HighlightCode(
+                InputText.Replace("&quot;", "\""),
+                "js", _rg)
+                );
+
+            // Display the highlighted code in a label control.
+            //Request.Form["GeneratedCode"] = CodeManager.Encode(
+            //CodeManager.HighlightHTMLCode(InputText, _htb)
+            //);
+            //ViewBag.GeneratedCode = CodeManager.Encode(
+            //CodeManager.HighlightHTMLCode(InputText, _htb)
+            //);
+
             return View();
         }
 
@@ -25,6 +49,51 @@ namespace FuncGeneretor.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult UpdateText(string text)
+        {
+            text += "\n";
+            string InputText = text + "var x = 10;\nx *= 5;\ndocument.getElementById(\"demo\").innerHTML = x;\nif( x == 8 ){\n}\nelse{\nwhile(true)//111test comment\n}\n//with new comment";
+            Hashtable _htb = CSASPNETHighlightCodeInPage.CodeManager.Init();
+
+            // Initialize the suitable collection object.
+            RegExp _rg = new RegExp();
+            _rg = (RegExp)_htb["js"];
+
+            // Display the highlighted code in a label control.
+            var result = CodeManager.Encode(
+                CodeManager.HighlightCode(
+                InputText.Replace("&quot;", "\""),
+                "js", _rg)
+                );
+
+            // Display the highlighted code in a label control.
+            //Request.Form["GeneratedCode"] = CodeManager.Encode(
+            //CodeManager.HighlightHTMLCode(InputText, _htb)
+            //);
+            //ViewBag.GeneratedCode = CodeManager.Encode(
+            //CodeManager.HighlightHTMLCode(InputText, _htb)
+            //);
+
+            return new JsonResult() { Data = result };
+        }
+
+        protected void btnHighLight_Click(object sender, EventArgs e)
+        {
+            string InputText = String.Format("{0}", Request.Form["GeneratedCode"]);
+            Hashtable _htb = CSASPNETHighlightCodeInPage.CodeManager.Init();
+
+            // Initialize the suitable collection object.
+            RegExp _rg = new RegExp();
+            _rg = (RegExp)_htb["js"];
+
+            // Display the highlighted code in a label control.
+            Request.Form["GeneratedCode"] = CodeManager.Encode(
+                CodeManager.HighlightHTMLCode(InputText, _htb)
+                );
+
         }
     }
 }
---
+++ FuncGeneretor/Models/HtmlTags.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FuncGeneretor.Models
+{
+    public class HtmlTags
+    {
+        public String ColoredCode{ get; set; }
+    }
+}
